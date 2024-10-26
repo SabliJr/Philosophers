49b5aa6539223cb21f8a1342ccde40d941136b8c
Sabli Junior Abakar Arbab <sabakar-@paul-f4Br3s1.clusters.42paris.fr>
2024-10-26 03:18:30 +0200

    M

--- philo/main.c
+++ philo/main.c
@@ -6,28 +6,48 @@
 /*   By: sabakar- <sabakar-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/25 22:16:32 by sabakar-          #+#    #+#             */
-/*   Updated: 2024/10/25 23:25:38 by sabakar-         ###   ########.fr       */
+/*   Updated: 2024/10/26 01:09:43 by sabakar-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./philo.h"
 
-int main (int ac, char *av[])
+int is_arg_valid(char **args, int ac)
 {
-    int x;
-    
-    if (ac < 5 || ac > 6)
-        return (printf("You have more than or less of the required args\n"), 1);
+    if ((ft_atoi(args[1]) > MAX_PHILOS) || ft_atoi(args[1]))
+        return ("Invaild Philosophers number\n", 0);
+    if (ft_atoi(args[2]) <= 0)
+        return (write(2, "Invalid time to die\n", 21), 0);
+    if (ft_atoi(args[3]) <= 0)
+        return (write(2, "Invalid time to eat\n", 21), 0);
+    if (ft_atoi(args[4]) <= 0)
+        return (write(2, "Invalid time to sleep\n", 23), 0);
+    if (args[5] && ft_atoi(args[5]) < 0)
+        return (write(2, "Invalid number of times each philosopher must eat\n",
+				51), 0);
 
-    if (!is_valid(av))
-        return (1);
-    
-    x = 1;
-    while (x < ac)
-    {
-        printf("The args: [%d]\n", ft_atoi(av[x]));
-        x++;
-    }
+    return (1);
+}
 
-    return (0);
+int	main(int ac, char *av[])
+{
+	int		x;
+	t_args	args[MAX_PHILOS];
+	t_philo	philo;
+    pthread_mutex_t les_threads[MAX_PHILOS];
+
+	if (ac < 5 || ac > 6)
+		return (printf("You have more than or less of the required args\n"), 1);
+        
+	if (!is_valid(av))
+		return (1);
+        
+	if (!is_arg_valid(av))
+		return (1);
+        
+	init_philo(&philo, args);
+    init_threads(les_threads, ft_atoi(av[1]));
+	init_args(args, philo, les_threads, av);
+    
+	return (0);
 }
